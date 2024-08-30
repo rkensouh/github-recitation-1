87006e55a84d06cfb6601b41738b7123a892bcc0
Reem Konsowa <reemkonsowa@Reems-MacBook-Air.local>
2024-08-30 12:29:17 +0300

    fixing the issue with fibRoute.ts file

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,11 +1,17 @@
-// Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import { Request, Response } from "express";
+import fibonacci from "./fib";
 
-export default (req, res) => {
+export default (req: Request, res: Response): void => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
+  const parsedNum = parseInt(num, 10);
+  if (isNaN(parsedNum)) {
+    res.status(400).send("Invalid number");
+    return;
+  }
+
+  const fibN = fibonacci(parsedNum);
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
